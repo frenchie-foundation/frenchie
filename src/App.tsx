4bca5000a920ca49691498c3a8e66e0dd6d2ae45
Davi Rodrigues <developer.davi@gmail.com>
2021-04-25 19:07:35 -0300

    wip: add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import HomePage from './pages/HomePage';
 import theme from './config/theme';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export const App: React.FC = () => (
   <ChakraProvider theme={theme}>
     <Header />
-    <HomePage />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </BrowserRouter>
     <Footer />
   </ChakraProvider>
 );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { Button, IconButton } from '@chakra-ui/button';
-import { Container, Flex, Stack } from '@chakra-ui/layout';
-import React, { useCallback } from 'react';
-import { FaTelegram } from 'react-icons/fa';
+import { Container, Flex, Stack, VStack } from '@chakra-ui/layout';
+import React, { useCallback, useState } from 'react';
+import { FaBars, FaTelegram } from 'react-icons/fa';
 
 import constants from '../config/constants';
 import openInNewTab from '../helpers/openInNewTab';
@@ -9,8 +9,35 @@ import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 import BscLogo from '../assets/bscscan-logo-circle.svg';
 import { Image } from '@chakra-ui/image';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+} from '@chakra-ui/modal';
+import { useHistory } from 'react-router';
 
 export default function Header(): React.ReactElement {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const history = useHistory();
+
+  const goTo = useCallback(
+    (path: string) => () => {
+      history.push(path);
+    },
+    []
+  );
+
+  const handleDrawerOpen = useCallback(() => {
+    setDrawerOpen(true);
+  }, []);
+
+  const handleDrawerClose = useCallback(() => {
+    setDrawerOpen(false);
+  }, []);
+
   const handlePancakeSwapLinkClick = useCallback(() => {
     openInNewTab(constants.pancakeSwapLink);
   }, []);
@@ -25,8 +52,33 @@ export default function Header(): React.ReactElement {
 
   return (
     <Container>
+      <Drawer placement="left" onClose={handleDrawerClose} isOpen={drawerOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton mt={2} />
+          <DrawerHeader borderBottomWidth="1px" mb={4}>
+            Menu
+          </DrawerHeader>
+          <DrawerBody>
+            <VStack spacing={4}>
+              <Button isFullWidth onClick={goTo('/')}>
+                Home page
+              </Button>
+              <Button isFullWidth onClick={goTo('/staking')}>
+                Staking
+              </Button>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
       <Flex pt={8} pb={8} justifyContent="space-between">
         <Stack spacing={2} direction="row">
+          <IconButton
+            backgroundColor="white"
+            aria-label="Menu"
+            icon={<FaBars color="black" />}
+            onClick={handleDrawerOpen}
+          />
           <Button colorScheme="red" onClick={handlePancakeSwapLinkClick}>
             Buy on PancakeSwap
           </Button>
